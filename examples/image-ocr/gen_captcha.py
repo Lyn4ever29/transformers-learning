04e558c04d8ffa2394a6af05a3ed43b673318992
lyn4ever29 <lyn4ever29@163.com>
2023-09-07 18:02:39 +0800

    添加检索式问答

--- examples/image-ocr/gen_captcha.py
+++ examples/image-ocr/gen_captcha.py
@@ -1,5 +1,6 @@
 import random
-from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from PIL import Image, ImageDraw, ImageFont
+import os
 
 
 def random_color():
@@ -7,7 +8,7 @@ def random_color():
 
 def random_char(length):
     # 随机选择字符
-    characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    characters = "023456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ"
     # 生成指定长度的验证码
     code = ''.join(random.choice(characters) for i in range(length))
     return code
@@ -23,14 +24,12 @@ def generate_captcha():
     code = random_char(4)
     for idx,t in enumerate(code):
         # "rtl", "ltr",
-        draw.text((40 * idx + 10, 10), t, font=font, fill=random_color(),direction='ltr')
+        draw.text((40 * idx + 10, 10), t, font=font, fill=random_color())
 
     # 生成干扰点
     for x in range(0,width,10):
         for y in range(0,height,10):
             draw.point((x, y), fill=(0,0,0))
-
-
 
     # 添加干扰线
     for i in range(3):
@@ -40,6 +39,13 @@ def generate_captcha():
         y2 = random.randint(0, height)
         draw.line((x1, y1, x2, y2), fill=(0, 0, 0), width=2)
     # image = image.filter(ImageFilter.GaussianBlur(radius=2))
-    image.save("captcha.jpg", "jpeg")
+    image.save(f"./data/{code}.jpg", "jpeg")
 
-generate_captcha()
+
+
+if not os.path.exists("./data"):
+    os.mkdir("./data")
+
+for i in range(1):
+    generate_captcha()
+# generate_captcha()
